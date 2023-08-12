cb2cd043b84a7e56c59f001b984f1e55b07c8a4a
EavCmr <vynyepouye@gmail.com>
2023-08-12 15:45:45 -0700

    0x01 C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*print number from 0 to 9*/
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i<10 ; i++)
+	{
+		printf("%d",i);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdioh>
+/*prints the lowercase in reverse, followed by new line*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z' ; ch>= 'a' ; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return(0);
+}
